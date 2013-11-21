eab2a907c143756617cd77b88f03e7055956d794
pablojimeno <pablojimeno@sonicbyte.com>
2013-11-21 10:20:33 +0100

    Add redis setup task

--- config/recipes/redis.rb
+++ config/recipes/redis.rb
@@ -4,4 +4,11 @@ namespace :redis do
     run "#{sudo} apt-get -y install redis-server"
   end
   after "deploy:install", "redis:install"
+
+  desc "Setup Redis"
+  task :setup, roles: :app do
+    run "mkdir -p #{shared_path}/config"
+    template "redis.yml.erb", "#{shared_path}/config/redis.yml"
+  end
+  after "deploy:setup", "redis:setup"
 end
